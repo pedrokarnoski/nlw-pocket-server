2e1652b06b23d2e28c17f77f4f835c998f64c648
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-09-14 14:47:41 -0300

    feat: Metas pendentes

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,11 +7,19 @@ import {
 
 import z from 'zod'
 import { createGoal } from '../use-cases/create-goal'
+import { getWeekPendingGoals } from '../use-cases/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
+
 
 app.post(
   '/goals',
